a08944716e5c0c9ea10abec128d56f0496459bed
tmwpwo <111870494+tmwpwo@users.noreply.github.com>
2023-03-07 13:41:21 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//my first GUI app written in go using fyne.io
+//bascially I used chucknorris api and this app diplays a chucknorris joke for every click of a button :DDD
+
 package main
 
 import (
